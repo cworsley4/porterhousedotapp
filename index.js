0a946f0186398b1c1dfdcb97ee32139b3eb8021f
Cecil Worsley <cecilworsley4@gmail.com>
2019-04-06 21:39:33 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const handler = require('serve-handler');
+const http = require('http');
+const port = process.env.PORT || 3000;
+ 
+const server = http.createServer((request, response) => {
+  return handler(request, response);
+});
+ 
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Running on port: ${port}`);
+});
