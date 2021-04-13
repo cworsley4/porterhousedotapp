db7d2fe05ac15b882a0df20e915fe65c80d31a07
Suan-Aik Yeo <yeosuanaik@gmail.com>
2021-04-12 21:49:56 -0400

    switch to express to support proxying

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const handler = require('serve-handler');
-const http = require('http');
+const express = require('express');
+var proxy = require('express-http-proxy');
+const app = express();
 const port = process.env.PORT || 3000;
 const download_url = "https://github.com/cworsley4/Porterhouse-releases/releases/download/v2.0.14/Porterhouse-2.0.14.dmg";
 
-const server = http.createServer((request, response) => {
-  return handler(request, response, {
-    "public": "build/pages/",
-    "cleanUrls": true,
-    "directoryListing": process.env.NODE_ENV === 'production' ? false : true,
-    "redirects": [
-      { "source": "/download", "destination": download_url, "type": 302 }
-    ],
-  });
+app.use(express.static('build/pages',{
+  extensions: ['html', 'htm'],
+}));
+
+app.get('/download', (req, res) => {
+  res.redirect(download_url);
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Running on port: ${port}`);
-});
+app.use('/blog', proxy('porterhouse.swish.ink', {
+  userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+    return {
+      ...headers,
+      'X-Forwarded-For': userReq.socket.remoteAddress,
+      'X-Forwarded-Proto': userReq.protocol,
+      'X-Real-IP': userReq.socket.remoteAddress,
+      'Host': userReq.headers.host,
+    };
+  },
+}));
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
