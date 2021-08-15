c961774ee88ae5393141926420be6b4c2cb73eaf
Suan-Aik Yeo <yeosuanaik@gmail.com>
2021-08-14 22:18:51 -0400

    2.1.4 for new users only

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3000;
-const download_url = "https://github.com/cworsley4/Porterhouse-releases/releases/download/v2.1.3/Porterhouse-2.1.3.dmg";
+// const download_url = "https://github.com/cworsley4/Porterhouse-releases/releases/download/v2.1.3/Porterhouse-2.1.3.dmg";
+const download_url = "https://storage.googleapis.com/public-builds/Porterhouse-2.1.4.dmg";
 
 app.use(express.static('build/pages',{
   extensions: ['html', 'htm'],
